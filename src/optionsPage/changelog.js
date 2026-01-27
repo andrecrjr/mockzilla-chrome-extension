2d521c9c1da13012a289c226781dd4fc51d833d9
André Carlos <andre-carlos@live.com>
2026-01-26 23:49:21 -0300

    feat: add changelog data for Mockzilla v0.3, detailing new features like Server Sync, Settings Modal, and Contact Form.

--- src/optionsPage/changelog.js
+++ src/optionsPage/changelog.js
@@ -7,7 +7,7 @@ export const changelogData = {
       {
         title: "🚀 New Features",
         items: [
-          "**Server Sync (BETA)**: Sync your rules to a Mockzilla server for team collaboration. [Learn more here](https://mockzilla.dev/docs#extension-sync)",
+          "**Server Sync (BETA)**: Sync your rules to a Mockzilla server for team collaboration. [Learn more here](https://mockzilla.dev/docs#extension-sync?utm_source=extension&utm_medium=changelog&utm_campaign=v0.3)",
           "**Settings**: Created a new Settings Modal UI to import, export rules and sync to server!",
           "**Contact Form**: Created a new Contact Form to send feedback to the developer aside Settings",
       ]
